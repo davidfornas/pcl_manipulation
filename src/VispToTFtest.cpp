4693e3b65c21ad7e40e9a404f7404d106ea2ab74
davidfornas <davidfornas@gmail.com>
2014-07-16 13:08:27 +0200

    Fixes

--- src/VispToTFtest.cpp
+++ src/VispToTFtest.cpp
@@ -12,28 +12,33 @@
 int main(int argc, char **argv) {
   ros::init(argc, argv, "test_visp_to_tf");
   ros::NodeHandle nh;
-  vpHomogeneousMatrix cMb(0,0.5,0,0,0,0), cMb2(0,0.5,-0.5,0,0,0), cMb3(1,1.5,-0.5,0,0,0), cMb4(3,1.5,-0.5,0,0,0);
+  vpHomogeneousMatrix cMb(0,0.5,0,0,0,0), cMb2(0,0.5,-0.5,0,0,0), cMb3(0,0,-0.5,1.57,1.57,0), cMb4(0,0,0.5,0,0,0);
   //(atof(argv[1]), atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]), atof(argv[6]));
   
   //VispToTF test
-  VispToTF v(cMb, "world", "something");
-  v.addTransform(cMb2, "something", "something_else","1");//No se está publicando
-  v.addTransform(cMb3, "world", "other_thing","2");
-  v.addTransform(cMb2, "world", "other_thing","6");
+  VispToTF v(cMb, "world", "zero");
+  v.addTransform(cMb2, "world", "disp","1");//No se está publicando
+  v.addTransform(cMb3, "world", "rotated","2");
+  v.addTransform(cMb3*cMb4, "world", "rotated_and_disp","6");
   
-  v.resetTransform(cMb4, "9");
+  //v.resetTransform(cMb4, "9");
   v.print();
   //Remove: v.removeTransform("1");
   
   ros::Rate rate(15);
   
   //VispToMarker test  
-  VispToMarker m(cMb2, "other_thing", "pose_marker", nh);
+  //MarkerPublisher m(cMb2, "other_thing", "pose_marker", nh);
+  ///tf::Vector vvv(0,0.5,0);
+  ///tf::Transform ttt;
+  ///MarkerPublisher m(pose, "other_thing", "pose_marker", nh);
+  
+  
   
   while(nh.ok()){
 
     v.publish();
-    m.publish();
+    ///m.publish();
     
     rate.sleep();
     ros::spinOnce();
--- src/save_cloud.cpp
+++ src/save_cloud.cpp
@@ -1,5 +1,5 @@
 /*
- * Copy a pointCloud2 msg to disk. May do some filtering.
+ * Copy a pointCloud2 msg to disk filtering to remove outliers.
  *
  *  Created on: 20/01/2013
  *      Author: dfornas
@@ -12,7 +12,6 @@
 
 // PCL specific includes
 #include <pcl_conversions/pcl_conversions.h>
-
 
 #include <pcl/conversions.h>
 #include <pcl/point_types.h>
@@ -45,37 +44,36 @@ void cloud_cb (const sensor_msgs::PointCloud2 input)
   std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
        << " data points (" << pcl::getFieldsList (*cloud) << ").";
 
-    // Perform the actual filtering
-    pcl::VoxelGrid< pcl::PCLPointCloud2 > vg;
-    vg.setInputCloud (cloud);
-    vg.setLeafSize (0.01, 0.01, 0.01);
-    vg.filter (*cloud_f1);
+  // Perform the actual filtering
+  pcl::VoxelGrid< pcl::PCLPointCloud2 > vg;
+  vg.setInputCloud (cloud);
+  vg.setLeafSize (0.01, 0.01, 0.01);
+  vg.filter (*cloud_f1);
     
   //Outlier removal
   pcl::StatisticalOutlierRemoval<pcl::PCLPointCloud2> sor;
   sor.setInputCloud (cloud_f1);
   sor.setMeanK (50);
   sor.setStddevMulThresh (1.0);
-  sor.filter (*cloud_f2);
+  sor.filter(*cloud_f2);
 
 
-//passthrough also possible
-//http://pointclouds.org/documentation/tutorials/reading_pcd.php#reading-pcd
+  //passthrough also possible
+  //http://pointclouds.org/documentation/tutorials/reading_pcd.php#reading-pcd
   //INVERSE OR NOT
   //for(int i=0;i<cloud->width;i++)
   //  cloud->data[i].z=-cloud->data[i].z;
 
-  std::cerr << "PointCloud before filtering: " << cloud_f1->width * cloud_f1->height 
-       << " data points (" << pcl::getFieldsList (*cloud_f1) << ").";
-//http://pointclouds.org/documentation/tutorials/reading_pcd.php#reading-pcd
+  std::cerr << "PointCloud after filtering: " << cloud_f2->width * cloud_f2->height 
+       << " data points (" << pcl::getFieldsList (*cloud_f2) << ").";
+  //http://pointclouds.org/documentation/tutorials/reading_pcd.php#reading-pcd
 
   std::string filename("output.pcd");
   pcl::io::savePCDFile(filename, *cloud_f1);
   vpImageIo::write(Ic, "output.ppm");
-  //Save camera parameters
   vpMatrix::saveMatrix("output.cam", vPtr->K.get_K());
 
-  ROS_INFO("The cloud is saved((not inversed)).");
+  ROS_INFO("The cloud is saved.");
 }
 
 int main (int argc, char** argv) {
@@ -83,11 +81,11 @@ int main (int argc, char** argv) {
   ros::init (argc, argv, "point_cloud_saver");
   ros::NodeHandle nh;
 
-  vPtr = VirtualImagePtr(new VirtualImage(nh,"camera/left/image_rect", "camera/left/camera_info", 1));
+  vPtr = VirtualImagePtr(new VirtualImage(nh,"stereo/left/image_rect", "stereo/left/camera_info", 1));
   vPtr->open(Ic) ;
   vPtr->acquire(Ic);
   // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe ("camera/points2", 1, cloud_cb);
+  ros::Subscriber sub = nh.subscribe ("stereo/points2", 1, cloud_cb);
 
   // Create a ROS publisher for the output point cloud
   //pub = nh.advertise<pcl::PCLPointCloud2> ("output", 1);
