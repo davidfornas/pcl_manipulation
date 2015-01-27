617ad037df083160b2c77fafb0c26d0b83773bad
David Fornas <davidfornas@gmail.com>
2015-01-27 10:00:10 +0100

    First version of tracking service

--- include/pcl_manipulation/arm5e_pc_grasp_planning_server.h
+++ include/pcl_manipulation/arm5e_pc_grasp_planning_server.h
@@ -1,26 +1,41 @@
 #ifndef GRASP_PLANNING_OBJECT_DETECTOR_SERVER
 #define GRASP_PLANNING_OBJECT_DETECTOR_SERVER
 
-
 #include "pcl_manipulation/DetectObject.h"
-#include "ros/ros.h"
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+//PCL Includes
+// PCL specific includes
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <pcl/conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
 
 namespace pcl_manipulation
 {
 
-class ObjectDetectorServer
+class ObjectDetectionServer
 {
 public:
 
-  ObjectDetectorServer(ros::NodeHandle& nh, ros::NodeHandle& nh_private);
+  ObjectDetectionServer(ros::NodeHandle& nh, ros::NodeHandle& nh_private);
 
 private:
 
   bool serviceCallback(DetectObjectRequest& req, DetectObjectResponse& res);
+  void cloudCallback(const sensor_msgs::PointCloud2 input);
 
   ros::NodeHandle nh_, nh_private_;
   ros::ServiceServer service_server_;
-  // planner pointer * MarkerDetector detector_;
+
+  ros::Subscriber sub_;
+
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_;
+
+
 };
 
 } // end of namespace
--- src/arm5e_pc_grasp_planning_server.cpp
+++ src/arm5e_pc_grasp_planning_server.cpp
@@ -1,30 +1,51 @@
 #include "pcl_manipulation/arm5e_pc_grasp_planning_server.h"
 
-pcl_manipulation::ObjectDetectorServer::ObjectDetectorServer(
+#include <mar_perception/PCAutonomousGraspPlanning.h>
+
+pcl_manipulation::ObjectDetectionServer::ObjectDetectionServer(
   ros::NodeHandle& nh, ros::NodeHandle& nh_private) :
   nh_(nh), nh_private_(nh_private)
 {
-  //detector_.loadSettings(nh_private_);
 
-  service_server_ = nh_private_.advertiseService("detect_marker",
-      &ObjectDetectorServer::serviceCallback, this);
-  ROS_INFO("Service \"detect_marker\" advertised.");
+  service_server_ = nh_private_.advertiseService("detect_object",
+      &ObjectDetectionServer::serviceCallback, this);
+  ROS_INFO("Service \"detect_object\" advertised.");
 }
 
-bool pcl_manipulation::ObjectDetectorServer::serviceCallback(
+bool pcl_manipulation::ObjectDetectionServer::serviceCallback(
     DetectObjectRequest& req,
     DetectObjectResponse& res)
 {
-  //detector_.setCameraInfo(req.camera_info, req.rectified);
-  //detector_.detect(req.image, res.markers);
+  ROS_INFO("OD called. Starting detection");
+  //subscribe to pointcloud...
+  sub_=nh_private_.subscribe("chatter", 1000, &ObjectDetectionServer::cloudCallback, this);
+  //wait for subscriber & cloud...
+  //pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  ROS_INFO("Start perception...");
+  PCAutonomousGraspPlanning planner(0, 0, 0, true, cloud_);
+  planner.perceive();
+  //cMg should be published in TF
+  //reachabillity and so on is done in client...
   return true;
 }
 
 
+void pcl_manipulation::ObjectDetectionServer::cloudCallback(const sensor_msgs::PointCloud2 input)
+{
+  //ROS Points2 message to PCL.
+  pcl::PCLPointCloud2 pcl_pc;
+  pcl_conversions::toPCL(input, pcl_pc);
+  //PCL Generic cloud to XYZRGB strong type.
+  pcl::fromPCLPointCloud2(pcl_pc, *cloud_);
+  ROS_INFO_STREAM("Acquiring cloud. Cloud size: " << cloud_->width * cloud_->height << "points.");
+  sub_.shutdown();
+}
 
-int main(void){
 
-
-
+int main(int argc, char **argv){
+  ros::init(argc, argv, "object_detection_server");
+  ros::NodeHandle nh;
+  pcl_manipulation::ObjectDetectionServer ods(nh,nh);
+  ros::spin();
   return 0;
 }
